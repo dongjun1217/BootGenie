d04c79a24496062269dcfae04f31037a4b406671
dongjun1217 <ehdwns9294@naver.com>
2024-05-19 03:35:08 +0900

    Initial commit

---
+++ src/main/java/com/bootgenie/app/BootGenieApplication.java
@@ -0,0 +1,13 @@
+package com.bootgenie.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGenieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootgenie/app/BootGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootgenie.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
