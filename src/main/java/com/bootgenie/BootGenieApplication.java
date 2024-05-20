0ad8389cc23fc03315288ff92d68c584b2e401d2
dongjun1217 <ehdwns9294@naver.com>
2024-05-21 04:02:45 +0900

    url 변경 및 파일 이동

--- src/main/java/com/bootgenie/BootGenieApplication.java
+++ src/main/java/com/bootgenie/BootGenieApplication.java
@@ -1,4 +1,4 @@
-package com.bootgenie.app;
+package com.bootgenie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/bootgenie/controller/ProjectController.java
+++ src/main/java/com/bootgenie/controller/ProjectController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.io.ByteArrayOutputStream;
 
 @RestController
-@RequestMapping("/api")
 public class ProjectController {
 
     @Autowired
