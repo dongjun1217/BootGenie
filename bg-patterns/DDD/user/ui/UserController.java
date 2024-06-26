87ccdbb18f79127e8b8af8be215bfb34a4f43941
dongjun1217 <ehdwns9294@naver.com>
2024-06-26 13:15:11 +0900

    Fix : Bug

--- bg-patterns/DDD/user/ui/UserController.java
+++ bg-patterns/DDD/user/ui/UserController.java
@@ -1,7 +1,7 @@
-package com.example.user.ui;
+package {BootGenie}.user.ui;
 
-import com.example.user.application.UserFacade;
-import com.example.user.domain.model.User;
+import {BootGenie}.user.application.UserFacade;
+import {BootGenie}.user.domain.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
